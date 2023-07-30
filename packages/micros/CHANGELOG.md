80c5baf92f4abaee73f596bb0724ecbd5dec2e48
krish7x <krishnakumar99g@gmail.com>
2023-07-30 23:23:54 +0530

    fix: add re-exports

--- packages/micros/CHANGELOG.md
+++ packages/micros/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @react-easy-styles/micros
 
+## 1.1.1
+
+### Patch Changes
+
+- Add re-exports for additional micros
+
 ## 1.1.0
 
 ### Minor Changes
--- packages/micros/index.tsx
+++ packages/micros/index.tsx
@@ -1,4 +1,9 @@
 export { default as Box } from './src/Box'
 export { default as Flex } from './src/Flex'
 export { default as Stack } from './src/Stack'
+export { default as Grid } from './src/Grid'
+export { default as Row } from './src/Row'
+export { default as Column } from './src/Column'
+
+
 
